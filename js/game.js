0a594f8f184a420690c3571b5b8d28ffaa657d94
ammerzone <jules.rau@gmx.de>
2017-09-30 21:53:09 +0200

    JS Documentiert & Bugfix

--- js/game.js
+++ js/game.js
@@ -1,204 +1,347 @@
+/* GLOBAL VARIABLES */
 var field = 	[], 
-	play = 		false, 
-	speed = 	0, 
-	size = 		0;
+	size = 		0, 
+	gameTimer = 0;
+/* END GLOBALS */
 
+// Init automattically the field (empty)
 $(initField);
 
+/**
+* Draws the field with the actual array-field
+* 
+* @access 	public
+* @return 	void
+* @see 		drawField()
+*/
 function drawField(){
+	
+	// Empty html element (field)
 	$('#game #game-field').html('');
 	
+	// For every y (maximum y = size)
 	for(var y = 0; y < size; y++){
 		
+		// For every x in a y (maximum x = size)
 		for(var x = 0; x < size; x++){
+			
+			// Append field-element to html-field
 			$('#game #game-field').append('<div class="field-element"></div>');
+			
+			// Add data-values (coordinates) to field element
 			$('#game #game-field').children().last('.field-element').attr('data-x', x);
 			$('#game #game-field').children().last('.field-element').attr('data-y', y);
+			
+			// Set style-size to field element
 			$('#game #game-field').children().last('.field-element').css({
 				width: 	(1 / size) * 100 + '%', 
 				height: (1 / size) * 100 + '%'
 			});
 			
+			// If this field is alive
 			if(field[x][y].status === 'alive'){
+				
+				// Add class 'alive' to field element
 				$('#game #game-field').children().last('.field-element').addClass('alive');
-			}else if(field[x][y].status === 'dead'){
-				$('#game #game-field').children().last('.field-element').addClass('dead');
 			}
 		}
 	}
 }
 
+/**
+* Initializes an empty field with actual size
+* 
+* @access 	public
+* @return 	void
+* @see 		initField()
+*/
 function initField(){
+	
+	// Get field size from html size-display
 	size = parseInt($('#game-btn-size').val());
 	
+	// For every x (maximum x = size)
 	for(var x = 0; x < size; x++){
+		
+		// Init empty x into field array
 		field[x] = [];
 		
+		// For every y (maximum y = size)
 		for(var y = 0; y < size; y++){ 
+		
+			// Init y in x-array with object-parameter status as 'dead'
 			field[x][y] = {
-				status: null
+				status: 'dead'
 			};    
 		}    
 	}
 	
+	// Draw the new field
 	drawField();
 }
 
+/**
+* Set a new field-size and draw the field
+* 
+* @access 	public
+* @param 	integer 	newSize
+* @return 	void
+* @see 		newSize()
+*/
 function newSize(newSize){
+	
+	// Set global size to new size
 	size = newSize;
 	
+	// Init a new temp vatiable for new field array
 	var temp = [];
+	
+	// For every x (maximum x = size)
 	for(var x = 0; x < size; x++){
+		
+		// Init empty x into temp array
 		temp[x] = [];
 		
+		// For every y (maximum y = size)
 		for(var y = 0; y < size; y++){
+			
+			// Check if actual x is in field-array
 			if(field.length > x){
+				
+				// Check if actual y is in field-array
 				if(field[x].length > y){
+					
+					// Check if actual field object has a status
 					if(field[x][y].status != 'undefined'){
+						
+						// Write actual field array object into temp array object
 						temp[x][y] = {
 							status: field[x][y].status
 						};
+						
+						// Go to next for-step
 						continue;
 					}
 				}
 			}
 			
+			// Initialize temp field object with status as 'dead'
 			temp[x][y] = {
-				status: null
+				status: 'dead'
 			};
 		}
 	}
 	
+	// Override field array with temp array
 	field = temp;
 	
+	// Draw the new field
 	drawField();
 }
 
-function newSpeed(newSpeed){
-	speed = newSpeed;
-}
-
+/**
+* Get the next field configuration and draw it
+* 
+* @access 	public
+* @return 	void
+* @see 		nextField()
+*/
 function nextField(){
+	
+	// Init a new temp vatiable for new field array
 	var temp = [];
 	
+	// For every x (maximum x = size)
 	for(var x = 0; x < field.length; x++){
+		
+		// Init empty x into temp array
+		temp[x] = [];
+		
+		// For every y (maximum y = size)
 		for(var y = 0; y < field[x].length; y++){
-			field[x][y].status = getNewStatus(x, y);
+			
+			// Write new field array object into temp array object
+			temp[x][y] = {
+				status: getNewStatus(x, y)
+			};
 		}
 	}
 	
+	// Override field array with temp array
+	field = temp;
+	
+	// Draw the new field
 	drawField();
+	
+	// Increase generation display
+	$('#game-generation').html(parseInt($('#game-generation').html()) + 1);
 }
 
+/**
+* Counts the amount of nighbors with staus that is 'alive'
+* 
+* @access 	public
+* @param 	integer 	x
+* @param 	integer 	y
+* @return 	integer
+* @see 		countNeighbors()
+*/
 function countNeighbors(x, y){
+	
+	// Init counter with value = 0
 	var counter = 0;
 	
-	if(x > 1){
-		if(y > 1){
-			if(field[x - 1][y - 1].status === 'alive'){
-				counter++;
-			}
-		}
-		
-		if(field[x - 1][y].status === 'alive'){
-			counter++;
-		}
-		
-		if(y < field[x - 1].length - 1){
-			if(field[x - 1][y + 1].status === 'alive'){
-				counter++;
-			}
-		}
+	// Get the left coordinate modulo size
+	minX = (x > 0) ? (x - 1) : (size - 1);
+	
+	// Get the top coordinate modulo size
+	minY = (y > 0) ? (y - 1) : (size - 1);
+	
+	// Get the right coordinate modulo size
+	maxX = (x < size - 2) ? (x + 1) : 0;
+	
+	// Get the bottom coordinate modulo size
+	maxY = (y < size - 2) ? (y + 1) : 0;
+	
+	// Check if left top neighbor is alive
+	if(field[minX][minY].status === 'alive'){
+		counter++;
 	}
 	
-	if(y > 1){
-		if(field[x][y - 1].status === 'alive'){
-			counter++;
-		}
-	}
-	
-	if(y < field[x].length - 1){
-		if(field[x][y + 1].status === 'alive'){
-			counter++;
-		}
-	}
-	
-	if(x < field.length - 1){
-		if(y > 1){
-			if(field[x + 1][y-1].status === 'alive'){
-				counter++;
-			}
-		}
+	// Check if left neighbor is alive
+	if(field[minX][y].status === 'alive'){
 		
-		if(field[x + 1][y].status === 'alive'){
-			counter++;
-		}
-		
-		if(y < field[x + 1].length - 1){
-			if(field[x + 1][y + 1].status === 'alive'){
-				counter++;
-			}
-		}
+		// Increase Counter
+		counter++;
 	}
 	
+	// Check if left right neighbor is alive
+	if(field[minX][maxY].status === 'alive'){
+		
+		// Increase Counter
+		counter++;
+	}
+	
+	// Check if top neighbor is alive
+	if(field[x][minY].status === 'alive'){
+		
+		// Increase Counter
+		counter++;
+	}
+	
+	// Check if bottom neighbor is alive
+	if(field[x][maxY].status === 'alive'){
+		
+		// Increase Counter
+		counter++;
+	}
+	
+	// Check if right top neighbor is alive
+	if(field[maxX][minY].status === 'alive'){
+		
+		// Increase Counter
+		counter++;
+	}
+	
+	// Check if right neighbor is alive
+	if(field[maxX][y].status === 'alive'){
+		
+		// Increase Counter
+		counter++;
+	}
+	
+	// Check if right bottom neighbor is alive
+	if(field[maxX][maxY].status === 'alive'){
+		
+		// Increase Counter
+		counter++;
+	}
+	
+	// Return number of counter
 	return counter;
 }
 
+/**
+* Gets the new status of a cell with the game rules 
+* 	Rule 1: Cell dies with less than 2 neighbors alive
+* 	Rule 2: Cell keeps alive with 2 or 3 neighbors alive
+* 	Rule 3: Cell dies with more than 3 neighbors alive
+* 	Rule 4: Dead cell transforms alive with exact 3 neighbors alive
+* 
+* @access 	public
+* @param 	integer 	x
+* @params 	integer 	y
+* @return 	void
+* @see 		getNewStatus()
+*/
 function getNewStatus(x, y){
+	
+	// Switch status of field element with x- and y-coordinate
 	switch(field[x][y].status){
 		case 'alive': {
-			if(countNeighbors(x, y) === 2 || countNeighbors(x, y) === 3){
-				
-				// Rule 1: Eine Zelle überlebt, wenn sie zwei oder drei lebendige Nachbarn hat.
-				return 'alive';
-			}else{
-				
-				// Rule 2: Eine lebendige Zelle stirbt, wenn sie weniger als zwei lebendige Nachbarzellen hat (Einsamkeit).
-				// Rule 3: Eine lebendige Zelle stirbt, wenn sie mehr als drei lebendige Nachbarzellen hat (Überbevölkerung).
-				return 'dead';
-			}
 			
-			break;
+			// Rule 2: Check if cell has two or three neighbors alive
+			return (countNeighbors(x, y) === 2 || countNeighbors(x, y) === 3) ? 'alive' : 'dead';
 		}
 		case 'dead': {
-			if(countNeighbors(x, y) === 3){
-				
-				// Rule 4: Eine Zelle wird wiederbelebt, wenn sie genau drei lebendige Nachbarn hat.
-				return 'alive';
-			}else{
-				
-				// Keeps dead
-				return 'dead';
-			}
 			
-			break;
+			// Rule 3: Check if dead cell has exact 3 neighbors alive
+			return (countNeighbors(x, y) === 3) ? 'alive' : 'dead';
 		}
-		case null: {
-			if(countNeighbors(x, y) === 2 || countNeighbors(x, y) === 3){
-				
-				// Rule 1: Eine Zelle überlebt, wenn sie zwei oder drei lebendige Nachbarn hat.
-				return 'alive';
-			}else{
-				
-				// Keep empty
-				return null;
-			}
-			break;
-		} 
 		default: {
-			if(countNeighbors(x, y) === 2 || countNeighbors(x, y) === 3){
-				
-				// Rule 1: Eine Zelle überlebt, wenn sie zwei oder drei lebendige Nachbarn hat.
-				return 'alive';
-			}else{
-				
-				// Keep empty
-				return null;
-			}
-			break;
+			
+			// Rule 2: Check if cell has two or three neighbors alive
+			return (countNeighbors(x, y) === 2 || countNeighbors(x, y) === 3) ? 'alive' : 'dead';
 		}
 	}
 	
-	// Keep empty
-	return null;
+	return 'dead';
+}
+
+/**
+* Starts the game loop
+* 
+* @access 	public
+* @return 	void
+* @see 		gamePlay()
+*/
+function gamePlay(){
+	
+	// Fill the gameTimer variable with the timeout, timer is load from actual speed display (calculates FPS to ms)
+	gameTimer = setTimeout(
+		function(){
+			// Build and show next field configuration
+			nextField();
+			
+			// Reload this function
+			gamePlay();
+		}, 
+		parseInt(1000 / parseFloat($('#game-btn-speed').val()))
+	);
+}
+
+/**
+* Clears the game loop Timeout and reset it to 0
+* 
+* @access 	public
+* @return 	void
+* @see 		gamePause()
+*/
+function gamePause(){
+	
+	// Check if gameTimer exists and is true
+	if(gameTimer){
+		
+		// Clear gameTimer
+		clearTimeout(gameTimer);
+		
+		// Set gameTimer to 0
+		gameTimer = 0;
+	}
+	
+	// Show start button
+	$('#game-btn-start').removeClass('hidden');
+	
+	// Hide pause button
+	$('#game-btn-pause').removeClass('hidden').addClass('hidden');
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// Trigger when move-button is clicked
 $(document).on('mousedown', '#game-button-move', function(e){
 	e.preventDefault();
 	
+	gamePause();
+	
+	// Make game box draggable
 	$('#game').draggable({
 		cursor: 		'move', 
 		snap: 			'#content-box', 
@@ -8,20 +12,25 @@ $(document).on('mousedown', '#game-button-move', function(e){
 		snapMode: 		'inner', 
 		disabled: 		false
 	});
+	
 	$('#game').addClass('ui-draggable-dragging');
 });
 
+// Trigger when move-button is released
 $(document).on('mouseup', '#game-button-move', function(e){
 	e.preventDefault();
 	
+	// Disable game box drag function
 	$('#game').draggable({
 		disabled: true
 	});
 });
 
+// Trigger when speed up button is clicked
 $(document).on('click', '#game-btn-speed-decrease', function(e){
 	e.preventDefault();
 	
+	// Set new speed to speed bar
 	$('#game-btn-speed').val(
 		Math.max(
 			parseFloat($('#game-btn-speed').attr('min')), 
@@ -29,14 +38,15 @@ $(document).on('click', '#game-btn-speed-decrease', function(e){
 		)
 	);
 	
+	// Update speed display
 	$('#game-speed').html($('#game-btn-speed').val());
-	
-	//
 });
 
+// Trigger when speed down button is clicked
 $(document).on('click', '#game-btn-speed-increase', function(e){
 	e.preventDefault();
 	
+	// Set new speed to speed bar
 	$('#game-btn-speed').val(
 		Math.min(
 			parseFloat($('#game-btn-speed').attr('max')), 
@@ -44,22 +54,23 @@ $(document).on('click', '#game-btn-speed-increase', function(e){
 		)
 	);
 	
+	// Update speed display
 	$('#game-speed').html($('#game-btn-speed').val());
-	
-	//
 });
 
+// Trigger when speed bar is changed
 $(document).on('change', '#game-btn-speed', function(e){
 	e.preventDefault();
 	
+	// Update speed display
 	$('#game-speed').html($('#game-btn-speed').val());
-	
-	//
 });
 
+// Trigger when field size decrease button is clicked
 $(document).on('click', '#game-btn-size-decrease', function(e){
 	e.preventDefault();
 	
+	// Set new field size to field size bar
 	$('#game-btn-size').val(
 		Math.max(
 			parseInt($('#game-btn-size').attr('min')), 
@@ -67,14 +78,18 @@ $(document).on('click', '#game-btn-size-decrease', function(e){
 		)
 	);
 	
+	// Update field size display
 	$('#game-size').html($('#game-btn-size').val() + 'x' + $('#game-btn-size').val());
 	
+	// Update field size variable
 	newSize($('#game-btn-size').val());
 });
 
+// Trigger when field size increase button is clicked
 $(document).on('click', '#game-btn-size-increase', function(e){
 	e.preventDefault();
 	
+	// Set new field size to field size bar
 	$('#game-btn-size').val(
 		Math.min(
 			parseInt($('#game-btn-size').attr('max')), 
@@ -82,73 +97,159 @@ $(document).on('click', '#game-btn-size-increase', function(e){
 		)
 	);
 	
+	// Update field size display
 	$('#game-size').html($('#game-btn-size').val() + 'x' + $('#game-btn-size').val());
 	
+	// Update field size variable
 	newSize($('#game-btn-size').val());
 });
 
+// Trigger when field size bar is changed
 $(document).on('change', '#game-btn-size', function(e){
 	e.preventDefault();
 	
+	// Update field size display
 	$('#game-size').html($('#game-btn-size').val() + 'x' + $('#game-btn-size').val());
 	
+	// Update field size variable
 	newSize($('#game-btn-size').val());
 });
 
+// Trigger when rule-button is clicked
 $(document).on('click', '#game-button-rules', function(e){
 	e.preventDefault();
 	
-	// pause
+	gamePause();
 	
-	// popup
-	$('game-popup').load('popup/rules.html');
+	// Open popup box
+	$('#game-popup').show().html(loadingSpinner());
+	
+	// Load rules content into box
+	$('#game-popup').load('popup/rules.html', function(){
+		$('#game-popup').hide().slideDown(400);
+		
+		// Append closing button to box
+		$('#game-popup').append(
+			'<center><a href="#" class="btn btn-lg btn-danger" id="game-button-close-popup">Close</a></center>'
+		);
+	});
 });
 
+// Trigger when configuration-button is clicked
 $(document).on('click', '#game-button-configurations', function(e){
 	e.preventDefault();
 	
-	// pause
+	gamePause();
 	
-	// popup
-	$('game-popup').load('popup/configurations.html');
+	// Open popup box
+	$('#game-popup').show().html(loadingSpinner());
+	
+	// Load configurations to box
+	$('#game-popup').load('popup/configurations.html', function(){
+		$('#game-popup').hide().slideDown(400);
+		
+		// Append closing button to box
+		$('#game-popup').append(
+			'<center><a href="#" class="btn btn-lg btn-danger" id="game-button-close-popup">Close</a></center>'
+		);
+	});
 });
 
+// Trigger when closing popup button is clicked
+$(document).on('click', '#game-button-close-popup', function(e){
+	e.preventDefault();
+	
+	// Close popup box
+	$('#game-popup').fadeOut(400, function(){
+		
+		// Empty popup box
+		$('#game-popup').html('');
+	});
+});
+
+// Trigger when clear button is clicked in game
 $(document).on('click', '#game-btn-clear', function(e){
 	e.preventDefault();
 	
+	gamePause();
 	
+	// Empty field
+	initField();
+	
+	// Reset generation display to 0
+	$('#game-generation').html('0');
 });
 
+// Trigger when next button is clicked in game
 $(document).on('click', '#game-btn-next', function(e){
 	e.preventDefault();
 	
+	gamePause();
+	
+	// Load next field view
 	nextField();
 });
 
+// Trigger when start button is clicked in game
 $(document).on('click', '#game-btn-start', function(e){
 	e.preventDefault();
 	
+	// Hide start button
 	$('#game-btn-start').removeClass('hidden').addClass('hidden');
+	
+	// Show pause button
 	$('#game-btn-pause').removeClass('hidden');
+	
+	// Start game loop
+	gamePlay();
 });
 
+// Trigger when pause button is clicked in game
 $(document).on('click', '#game-btn-pause', function(e){
 	e.preventDefault();
 	
-	$('#game-btn-start').removeClass('hidden');
-	$('#game-btn-pause').removeClass('hidden').addClass('hidden');
+	gamePause();
 });
 
+// Trigger when clicked in a field element in game
 $(document).on('click', '.field-element', function(e){
 	e.preventDefault();
 	
+	// Check if clicked element is alive
 	if($(this).hasClass('alive')){
+		
+		// Remove alive (kill)
 		$(this).removeClass('alive');
 		
-		field[$(this).attr('data-x')][$(this).attr('data-y')].status = '';
+		// Update element to dead in array
+		field[$(this).attr('data-x')][$(this).attr('data-y')].status = 'dead';
 	}else{
+		
+		// Add alive
 		$(this).addClass('alive');
 		
+		// Add element to alive in array
 		field[$(this).attr('data-x')][$(this).attr('data-y')].status = 'alive';
 	}
 });
+
+// Function to get HTML loading spinner
+function loadingSpinner(){
+	var html = '';
+	html += '<div class="spinner-circle">';
+	html += 	'<div class="spinner-circle1 spinner-child"></div>';
+	html += 	'<div class="spinner-circle2 spinner-child"></div>';
+	html += 	'<div class="spinner-circle3 spinner-child"></div>';
+	html += 	'<div class="spinner-circle4 spinner-child"></div>';
+	html += 	'<div class="spinner-circle5 spinner-child"></div>';
+	html += 	'<div class="spinner-circle6 spinner-child"></div>';
+	html += 	'<div class="spinner-circle7 spinner-child"></div>';
+	html += 	'<div class="spinner-circle8 spinner-child"></div>';
+	html += 	'<div class="spinner-circle9 spinner-child"></div>';
+	html += 	'<div class="spinner-circle10 spinner-child"></div>';
+	html += 	'<div class="spinner-circle11 spinner-child"></div>';
+	html += 	'<div class="spinner-circle12 spinner-child"></div>';
+	html += '</div>';
+	
+	return html;
+}
